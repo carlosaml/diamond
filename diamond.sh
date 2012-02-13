73d3e6847c0ec63036ebb81c5acd138cf5ddba5d
Carlos Lopes <carlosaml@Carlos-TW-MacBook-Pro.local>
2012-02-13 18:23:37 -0200

    it WOOOKS

--- diamond.sh
+++ diamond.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-
-# chr() - converts decimal value to its ASCII character representation
-# ord() - converts ASCII character to its decimal value
-
 chr() {
   printf \\$(printf '%03o' $1)
 }
@@ -14,22 +10,54 @@ ord() {
 
 ord_a=$(ord A)
 number=$(ord $1)
+next_to_last=$(($number-1))
 
 default_diff=$(($number - $ord_a)) 
 
 diff_to_a=0
-#echo $diff_to_a
 
 i=65
 while [ $i -le $number ]
 do
-
-
   j=$(($number-$i))
   left=j
   while [ $j -gt 0 ]
   do
-    printf '.'
+    printf ' '
+    j=$(($j-1))
+  done  
+
+  chr $i
+
+  diff_to_a=$(( $i - $ord_a)) 
+
+  spaces_at_right=0
+  if [ $diff_to_a -ne "0" ]; then
+  
+   spaces_at_right=$(($diff_to_a * 2 + $left - 1));
+
+    while [ $spaces_at_right -gt 0 ]
+    do
+      printf ' '
+      spaces_at_right=$(($spaces_at_right-1))
+    done
+
+    chr $i    
+  fi
+
+  i=$(( $i + 1 ))
+
+  echo
+done
+
+i=$next_to_last
+while [ $i -ge $ord_a ]
+do
+  j=$(($number-$i))
+  left=j
+  while [ $j -gt 0 ]
+  do
+    printf ' '
     j=$(($j-1))
   done  
 
@@ -45,7 +73,7 @@ do
 
     while [ $spaces_at_right -gt 0 ]
     do
-      printf '.'
+      printf ' '
       spaces_at_right=$(($spaces_at_right-1))
     done
 
@@ -53,8 +81,7 @@ do
   fi
 
 
-  i=$(( $i + 1 ))
+  i=$(( $i - 1 ))
 
   echo
 done
-
